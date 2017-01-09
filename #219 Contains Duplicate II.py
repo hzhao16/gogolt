60aa75c9c88f3c494f9a591d1d32ab4ce5844ab9
hzhao16 <econzh@gmail.com>
2017-01-09 22:50:51 +0800

    Array

---
+++ #219 Contains Duplicate II.py
@@ -0,0 +1,18 @@
+#219 Contains Duplicate II
+
+'''
+Given an array of integers and an integer k, 
+find out whether there are two distinct indices i and j in the array 
+such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+'''
+
+# hash
+def containsNearbyDuplicate(nums, k):
+    n = len(nums)
+    d = {}
+    for i in range(n):
+        if d.get(nums[i],None)!=None and i-d[nums[i]]<=k:
+            return True
+        d[nums[i]] = i            
+    return False
