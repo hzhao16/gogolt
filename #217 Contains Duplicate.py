d56fe3799fb1fd8974dca7e203a10c6ec9e9f388
hzhao16 <econzh@gmail.com>
2017-01-11 12:15:28 +0800

    Hash

--- #217 Contains Duplicate.py
+++ #217 Contains Duplicate.py
@@ -4,7 +4,7 @@
 Your function should return true if any value appears at least twice in the array,
  and it should return false if every element is distinct.
 '''
-# ***hash table***
+# ***hash table**
 def containsDuplicate(nums):
     n = len(nums)
     d = {}
